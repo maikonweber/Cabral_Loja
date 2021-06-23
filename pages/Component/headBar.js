a590fcc1216504afaa6b534dab4d21e1057d4a82
Maikon <maikonweber@gmail.com>
2021-06-22 23:49:56 -0300

    Iniciando o Projeto

---
+++ pages/Component/headBar.js
@@ -0,0 +1,9 @@
+const Headbar = () => {
+
+    return ( 
+        <div>
+            Ola Mundo
+        </div>
+    )}
+
+export default Headbar 
---
+++ pages/Component/sideBar.js
@@ -0,0 +1,11 @@
+import styles from '../../styles/slidebar.module.css';
+
+const Slidebar = () => {
+
+    return ( 
+        <div>
+            Slide
+        </div>
+    )}
+
+export default Slidebar 
